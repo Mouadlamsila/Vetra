85068f5939c6c0602034eaa84813001783fad50d
belkahla-mohamed <dev.mohamedbelkahla@gmail.com>
2025-04-23 23:14:06 +0100

    fix boardTable page language

---
+++ frontend/src/i18n/languages/index.js
@@ -0,0 +1,17 @@
+import ar from './ar';
+import en from './en';
+import fr from './fr';
+
+const resources = {
+  ar: {
+    translation: ar
+  },
+  en: {
+    translation: en
+  },
+  fr: {
+    translation: fr
+  }
+};
+
+export default resources; 
